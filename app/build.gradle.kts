6d4d2740335922225bc2ce462c05020e3af8d64f
Valentin Gamas Maldonado <valentin_gamas@soy.sena.edu.co>
2023-12-09 22:10:00 -0500

    Validacion de campos y escritura chambona en bd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,4 +40,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     // implementation("com.google.firebase:firebase-auth-ktx")
+    // implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    // implementation("com.google.firebase:firebase-analytics")
+
 }
--- app/src/main/java/com/example/helloworld/Administrador/RegistroAdmin.java
+++ app/src/main/java/com/example/helloworld/Administrador/RegistroAdmin.java
@@ -1,22 +1,31 @@
 package com.example.helloworld.Administrador;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.helloworld.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseUser;
 //import com.google.firebase.auth.FirebaseAuth;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 
 /* *
  * A simple {@link Fragment} subclass.
@@ -28,7 +37,7 @@ public class RegistroAdmin extends Fragment {
     TextView FechaRegistro;
     EditText Correo, Password, Nombres, Apellidos, FechaNacimiento;
     Button RegistrarAdministrador;
-//    FirebaseAuth auth;
+    FirebaseAuth auth;
     ProgressDialog progressDialog;
 /*
     // TODO: Rename parameter arguments, choose names that match
@@ -94,15 +103,99 @@ public class RegistroAdmin extends Fragment {
 //        auth = FirebaseAuth.getInstance();
 
         Date date = new Date();
-
         SimpleDateFormat fecha = new SimpleDateFormat("d'/'MM'/'yyyy");
 
         String Sfecha = fecha.format(date);
         FechaRegistro.setText(Sfecha);
 
-        // Inflate the layout for this fragment
+        RegistrarAdministrador.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                String correo = Correo.getText().toString();
+                String pass = Password.getText().toString();
+                String nombres = Nombres.getText().toString();
+                String apellidos = Apellidos.getText().toString();
+                String nacimiento = FechaNacimiento.getText().toString();
+                String tipoUsuario = "A";
+
+                if (correo.isEmpty() || pass.isEmpty() || nombres.isEmpty() || apellidos.isEmpty() || nacimiento.isEmpty()) {
+                    Toast.makeText(getActivity(),"Por favor llenar todos los campos", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    if (!Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
+                        Correo.setError("Correo inválido");
+                        Correo.setFocusable(true);
+                    } else if (pass.length() < 6) {
+                        Password.setError("La contraseña debe tener mínimo 6 caracteres");
+                        Password.setFocusable(true);
+                    } else {
+                        AlmacenarAdministrador(correo, pass); // no me gusta estos parametros
+                    }
+                }
+            }
+        });
+
+        progressDialog = new ProgressDialog(getActivity());
+        progressDialog.setMessage("Registrando, espere por favor");
+        progressDialog.setCancelable(false);
+
         return view;
     }
+
+    private void AlmacenarAdministrador(String email, String clave) {
+
+        progressDialog.show();
+
+        // com.google.android.firebase.aut.FirebaseAuth
+        auth.createUserWithWmailAndPassword(email, clave).addOnCompleteListener(new OncompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progressDialog.dismiss();
+                    FirebaseUser user = auth.getCurrentUser();
+                    assert user != null;
+
+                    String UID = user.getUid();
+                    String correo = Correo.getText().toString();
+                    String pass = Password.getText().toString();
+                    String nombres = Nombres.getText().toString();
+                    String apellidos = Apellidos.getText().toString();
+                    String nacimiento = FechaNacimiento.getText().toString();
+                    String tipoUsuario = "A";
+
+                    HashMap UsrAdministrador = new HashMap();
+                    UsrAdministrador.put("UID", UID); // Generar un userid de alguna manera que no sea creando el registro en firebase
+                    UsrAdministrador.put("CORREO", correo);
+                    UsrAdministrador.put("PASSWORD", pass);
+                    UsrAdministrador.put("NOMBRES", nombres);
+                    UsrAdministrador.put("APELLIDOS", apellidos);
+                    UsrAdministrador.put("FECHANACIMIENTO", nacimiento);
+                    UsrAdministrador.put("TIPO", tipoUsuario);
+
+                    FirebaseDatabase database = FirebaseDatabase.getInstance();
+                    DatabaseReference reference = database.getReference("TIENDA CELULARES");
+
+                    // escribir en la coleccion de administradores y/o de usuarios
+
+                    reference.child(UID).setValue(UsrAdministrador);
+
+                    startActivity(new Intent(getActivity(), MainActivityAdministrador.class));
+                    Toast.makeText(getActivity(),"Administrador registrado exitosamente", Toast.LENGTH_LONG).show();
+                    getActivity().finish();
+                }
+                else {
+                    progressDialog.dismiss();
+                    Toast.makeText(getActivity(), "Error al registrar el administrador", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getActivity(),e.getMessage(),Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 
 
 /*    @Override
